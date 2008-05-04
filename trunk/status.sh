aad3ff2c63789df77857f6e28d2ee42768c5df55
quinn.liqin <quinn.liqin@b26b1a16-034c-0410-a32b-b537de91d521>
2008-05-04 16:43:49 +0000

    small fix

--- trunk/status.sh
+++ trunk/status.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo ==== ip link \| grep mit ====
+ip link | grep mit
 echo ==== ip tunnel -n ====
 ip tunnel
 echo ==== ip route -n ====
@@ -6,4 +8,7 @@ ip route
 echo ==== ip rule -n ====
 ip rule
 echo ==== arp -n ====
-arp -n
+arp -n 2> /dev/null
+echo ==== sysctl -a \| grep proxy_arp ====
+sysctl -a 2> /dev/null | grep proxy_arp
+

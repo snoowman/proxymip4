9f9e9835c10f811615cd179c1b8ceea7be43c7ee
quinn.liqin <quinn.liqin@b26b1a16-034c-0410-a32b-b537de91d521>
2008-05-15 19:20:25 +0000

    split scripts

--- src/ha.cpp
+++ src/ha.cpp
@@ -85,8 +85,15 @@ private:
     struct pmip_nonskip *nonskip = (struct pmip_nonskip *)p;
     nonskip->type    = MIPEXT_PMIPNOSK;
     nonskip->subtype = PMIPNOSK_AUTH;
-    nonskip->length  = htons(1);
     nonskip->method  = PMIPAUTH_FAHA;
+
+    /*
+     * in pmip4 draft version, length are u16, so the following line would be
+     *   nonskip->length  = htons(1);
+     * but I decide to use u8 instead, bit me
+     */
+    nonskip->length  = 1;
+
     p += sizeof(*nonskip);
 
     /* add VM home correspondent nodes externsion */
@@ -239,7 +246,13 @@ private:
         {
           struct pmip_nonskip *nonskip = (struct pmip_nonskip *)p;
           p += sizeof(*nonskip);
-          p += ntohs(nonskip->length) - 1;
+
+          /*
+           * in pmip4 draft version, length are u16, so the following line would be
+           *   p += ntohs(nonskip->length) - 1;
+           * but I decide to use u8 instead, bit me
+           */
+          p += nonskip->length - 1;
   
           if (nonskip->subtype != PMIPNOSK_AUTH) {
             syslog(LOG_WARNING, "unknown non-skippable subtype %hhd\n", nonskip->subtype);

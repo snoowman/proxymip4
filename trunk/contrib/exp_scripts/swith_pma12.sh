4bb4d1fa0ff93d339c3baba6599aa568263a21eb
quinn.liqin <quinn.liqin@b26b1a16-034c-0410-a32b-b537de91d521>
2008-05-15 19:43:56 +0000

    split scripts, cont

--- trunk/contrib/exp_scripts/swith_pma12.sh
+++ trunk/contrib/exp_scripts/swith_pma12.sh
@@ -2,7 +2,7 @@
 ID=`xm list | grep mnode | awk '{print $2}'`
 if [ -n $ID ]
 then
-  brctl delif xenbr1 vif$ID.0
+  brctl delif xenbr2 vif$ID.0
   brctl addif xenbr3 vif$ID.0
 
   ssh pmagent2 /usr/sbin/pmip4-pma -m 192.168.101.3 -h 192.168.101.1 -c 192.168.100.3 -i eth1 -s 1000 -r
--- trunk/contrib/exp_scripts/swith_pma21.sh
+++ trunk/contrib/exp_scripts/swith_pma21.sh
@@ -2,7 +2,7 @@
 ID=`xm list | grep mnode | awk '{print $2}'`
 if [ -n $ID ]
 then
-  brctl delif xenbr1 vif$ID.0
+  brctl delif xenbr3 vif$ID.0
   brctl addif xenbr2 vif$ID.0
   ssh pmagent1 /usr/sbin/pmip4-pma -m 192.168.101.3 -h 192.168.101.1 -c 192.168.100.2 -i eth1 -s 1000 -r
 fi
